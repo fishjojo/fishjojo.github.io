8c69c3a03c9e79d55e4b3d37d17e23bc7b5ee690
Xing Zhang <zhangxing.nju@gmail.com>
2019-08-09 19:33:23 -0400

    update

--- js/load_html.js
+++ js/load_html.js
@@ -1,3 +1,15 @@
 function load_html(url,id){
   $("#"+id).load(url);
 }
+
+function set_bg(id,name){
+
+  pic_url =  "/pics/"+name+"_bg.JPG"
+  day = new Date()
+  hour = day.getHours()
+  if(hour > 17){
+    pic_url = "/pics/homepage"+name+"_bg_night.JPG"
+  }
+
+  document.getElementById(id).style.backgroundImage = "url('"+pic_url +"')";
+}
